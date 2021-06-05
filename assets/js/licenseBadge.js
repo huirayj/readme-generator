f96e8bd61b034708ddf33079837066d849b10f51
Justin Hui <huirayj@gmail.com>
2021-06-05 14:12:35 -0400

    updated licenseBadge

--- assets/js/licenseBadge.js
+++ assets/js/licenseBadge.js
@@ -1,5 +1,5 @@
 const licenseBadge = (license) => {
-    switch (license) {
+    switch(license) {
         case 'Apache 2.0 License':
             return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
             break;
