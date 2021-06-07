412c3337bd7c6ba231b8e043b879d4376107deba
Justin Hui <huirayj@gmail.com>
2021-06-07 19:00:22 -0400

    added comments

--- assets/js/handlers.js
+++ assets/js/handlers.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const generateMarkdown = require('./generateMarkdown');
 
+// creates a file called 'newREADME.md' and populates it
 const responseHandler = (response) => {
     fs.writeFile('newREADME.md', generateMarkdown(response), (err) =>
         console.error(err ? err : 'Your README.md was successfully created.'));
--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const inquirer = require('inquirer');
 const questions = require('./assets/js/questions');
 const handlers = require('./assets/js/handlers');
 
+// prompts user response and process the inputed data
 inquirer.prompt(questions)
         .then(handlers.responseHandler);
