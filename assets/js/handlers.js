942c67f98f5aef2bf660894e5cc34088d5384f68
Justin Hui <huirayj@gmail.com>
2021-06-07 16:18:58 -0400

    change function format

--- assets/js/handlers.js
+++ assets/js/handlers.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const generateMarkdown = require('./generateMarkdown');
 
 const responseHandler = (response) => {
-    // const data = JSON.stringify(response, null, 2);
-
-    fs.writeFile('newREADME.md', generateMarkdown(response), (err) => console.error(err ? err : 'Your README.md was successfully created.'));
+    fs.writeFile('newREADME.md', generateMarkdown(response), (err) =>
+        console.error(err ? err : 'Your README.md was successfully created.'));
 }
 
 module.exports = { responseHandler };
